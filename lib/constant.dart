490e2e0f9c95783872cf014baa97b4e1c1575124
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-06-11 22:24:24 +0530

    calculator app added

---
+++ lib/constant.dart
@@ -0,0 +1,8 @@
+
+
+import 'package:flutter/material.dart';
+
+const Color tealColor = Colors.teal;
+const Color whiteColor = Colors.white;
+
+const headingTextStyle = TextStyle( color: whiteColor,);
